5915ef4a51efcbcbb3e1e1ee81d4c17bc113db2b
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-06-10 21:26:02 -0300

    Project product-api

---
+++ product-api/src/main/java/com/microservice/productapi/controller/StatusController.java
@@ -0,0 +1,25 @@
+package com.microservice.productapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("api/status")
+public class StatusController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<HashMap<String, Object>> status(){
+        HashMap<String, Object> status = new HashMap<>();
+        status.put("service", "Product-API");
+        status.put("status", "up");
+        status.put("httpStatus", HttpStatus.OK);
+        return ResponseEntity.ok(status);
+    }
+}
